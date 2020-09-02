f5bd90576a92aa752a4cccaf914f97a1444f5b7c
Lucas Smith <lucas@wizardit.tech>
2020-09-02 16:12:49 -0400

    Adding Scripts

---
+++ AzureAD/1_User Set Password to Expire.ps1
@@ -0,0 +1,22 @@
+﻿$User = Read-Host -Prompt 'Input user email'
+cls
+
+$Credential = Get-Credential -Message "Input Service Account Credentials"
+$Credential.password.MakeReadOnly()
+Connect-AzureAD -Credential $Credential | Out-Null
+
+cls
+Write-Host "Setting Password to xpire for: $User"
+Write-Host " "
+
+Set-AzureADUser -ObjectId $User -PasswordPolicies None | Out-Null
+
+Write-Host "PasswordNeverExpires should read False..."
+Write-Host " "
+
+Get-AzureADUser -ObjectId $User | Select-Object UserprincipalName,@{
+    N="PasswordNeverExpires";E={$_.PasswordPolicies -contains "DisablePasswordExpiration"}
+} | Out-Host
+
+
+read-host "Press ENTER to exit..."
---
+++ AzureAD/2_User Set Password to Not Expire.ps1
@@ -0,0 +1,21 @@
+﻿$User = Read-Host -Prompt 'Input user email'
+cls
+
+$Credential = Get-Credential -Message "Input Service Account Credentials"
+$Credential.password.MakeReadOnly()
+Connect-AzureAD -Credential $Credential | Out-Null
+
+Write-Host "Setting Password to never expire for: $User"
+Write-Host " "
+
+Set-AzureADUser -ObjectId $User -PasswordPolicies DisablePasswordExpiration | Out-Null
+
+Write-Host "PasswordNeverExpires should read True..."
+Write-Host " "
+
+Get-AzureADUser -ObjectId $User | Select-Object UserprincipalName,@{
+    N="PasswordNeverExpires";E={$_.PasswordPolicies -contains "DisablePasswordExpiration"}
+} | Out-Host
+
+
+read-host "Press ENTER to exit..."
